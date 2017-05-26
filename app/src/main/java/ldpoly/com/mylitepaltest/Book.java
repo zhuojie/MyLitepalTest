b4f9606c2093fe213b17f151f114b4d23a933982
zj <1103751903@qq.com>
2017-05-26 22:50:59 +0800

    Initial commit

---
+++ app/src/main/java/ldpoly/com/mylitepaltest/Book.java
@@ -0,0 +1,49 @@
+package ldpoly.com.mylitepaltest;
+
+public class Book {
+    private int id;
+    private String author;
+    private String name;
+    private int pages;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
